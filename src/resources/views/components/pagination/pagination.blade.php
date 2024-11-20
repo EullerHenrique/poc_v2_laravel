49e7e58f76759b4044d1f5409ddd21c2d94dac70
Euller Henrique Bandeira Oliveira <euller.oliveira@arphoenix.com.br>
2024-11-19 22:13:37 -0300

    feat: Ajustes Front-End

---
+++ src/resources/views/components/pagination/pagination.blade.php
@@ -0,0 +1,9 @@
+<div class="col d-flex justify-content-center">
+    <div class="mt-4 mb-2">
+        @foreach($links as $link)
+            <a href="{{ $link['url'] }}" class = "btn {{ $link['active']? 'btn-primary' : 'btn-outline-primary' }}">
+                {{ $link['label'] }}
+            </a>
+        @endforeach
+    </div>
+</div>
